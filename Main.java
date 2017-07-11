905fe974f4d1929ea8988a19bfed34013c6b3f7c
Viktor Citaku <viktor.citaku@gmail.com>
2017-07-11 22:30:05 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,26 +11,25 @@ import java.util.Set;
 public class Main {
 
   public static void main(String[] args) {
-    try(BufferedReader br = new BufferedReader(new FileReader("./in"));
+    try (BufferedReader br = new BufferedReader(new FileReader("./in"));
         final BufferedWriter bw = new BufferedWriter(new FileWriter("./out"))) {
       Map<String, Integer> wordsRepeated = new HashMap<>();
       String sentence;
       while ((sentence = br.readLine()) != null) {
         String[] words = sentence.split(" ");
         for (String word : words) {
-          wordsRepeated.put(word, wordsRepeated.get(word) == null ? 1 : wordsRepeated.get(word) + 1);
+          wordsRepeated.put(word,
+              wordsRepeated.get(word) == null ? 1 : wordsRepeated.get(word) + 1);
         }
       }
       Set<Entry<String, Integer>> entries = wordsRepeated.entrySet();
-      entries.stream()
-        .filter(x -> x.getValue() > 1)
-        .forEach(x -> {
-          try {
-            bw.write("Word: " + x.getKey() + " | Appeared: " + x.getValue() + " times\n");
-          } catch (IOException ex) {
-            System.err.println(ex.getMessage());
-          }
-        });
+      entries.stream().filter(x -> x.getValue() > 1).forEach(x -> {
+        try {
+          bw.write("Word: " + x.getKey() + " | Appeared: " + x.getValue() + " times\n");
+        } catch (IOException ex) {
+          System.err.println(ex.getMessage());
+        }
+      });
     } catch (IOException ex) {
       System.out.println(ex.getMessage());
     }
